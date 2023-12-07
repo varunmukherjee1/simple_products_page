2bfc2e51acc296f97971382eea8aa4af12a36ee4
Varun <varunmukherjee5@gmail.com>
2023-12-07 18:41:19 +0530

    basic structure built

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from "./navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from "./products.module.css"
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
