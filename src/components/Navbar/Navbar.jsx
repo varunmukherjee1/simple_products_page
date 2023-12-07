aca2e61d7868cadaeae54d62463299d0230fdc9e
Varun <varunmukherjee5@gmail.com>
2023-12-07 22:25:22 +0530

    navbar modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 import classes from "./navbar.module.css"
 import logo from "../../assets/logo.png"
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const onClickHandler = () => {
+    navigate(`/`)
+  }
+
   return (
-    <div className = {classes.box}>
+    <div className = {classes.box} onClick={onClickHandler}>
       <div className = {classes.container}>
         <div className = {classes.logo}>
           <img src= {logo} alt="Logo" />
